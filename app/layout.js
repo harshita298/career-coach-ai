5f6344f22218e97c739a211a6c49d4f50f81cec4
Harshita Rajput <harshitarajput298@gmail.com>
2025-04-29 20:38:37 +0530

    Preparing for Deployment

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,52 @@
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import { ClerkProvider } from "@clerk/nextjs";
+// import { Toaster } from "sonner";
+// import Header from "@/components/header";
+// import { ThemeProvider } from "@/components/theme-provider";
+// import { dark } from "@clerk/themes";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata = {
+//   title: "AI Career Coach",
+//   description: "",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <ClerkProvider
+//       appearance={{
+//         baseTheme: dark,
+//       }}
+//     >
+//       <html lang="en" suppressHydrationWarning>
+//         <head>
+//           <link rel="icon" href="/logo.png" sizes="any" />
+//         </head>
+//         <body className={`${inter.className}`}>
+//           <ThemeProvider
+//             attribute="class"
+//             defaultTheme="dark"
+//             enableSystem
+//             disableTransitionOnChange
+//           >
+//             <Header />
+//             <main className="min-h-screen">{children}</main>
+//             <Toaster richColors />
+
+//             <footer className="bg-muted/50 py-12">
+//               <div className="container mx-auto px-4 text-center text-gray-200">
+                
+//               </div>
+//             </footer>
+//           </ThemeProvider>
+//         </body>
+//       </html>
+//     </ClerkProvider>
+//   );
+// }
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -24,7 +73,8 @@ export default function RootLayout({ children }) {
         <head>
           <link rel="icon" href="/logo.png" sizes="any" />
         </head>
-        <body className={`${inter.className}`}>
+        <body className={`${inter.className} bg-background text-foreground`}>
+          <div className="grid-background" />
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
@@ -34,10 +84,9 @@ export default function RootLayout({ children }) {
             <Header />
             <main className="min-h-screen">{children}</main>
             <Toaster richColors />
-
-            <footer className="bg-muted/50 py-12">
+            <footer className="bg-muted py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by RoadsideCoder</p>
+                
               </div>
             </footer>
           </ThemeProvider>
--- components/hero.jsx
+++ components/hero.jsx
@@ -31,13 +31,12 @@ const HeroSection = () => {
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
           <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
-            Your AI Career Coach for
-            <br />
-            Professional Success
+            AI Career Compass 
+            {/* <br />
+            Professional Success */}
           </h1>
           <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
-            Advance your career with personalized guidance, interview prep, and
-            AI-powered tools for job success.
+          Harness AI to Accelerate Your Professional Growth
           </p>
         </div>
         <div className="flex justify-center space-x-4">
@@ -46,16 +45,16 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          {/* <Link href="https://www.youtube.com/roadsidecoder">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
-          </Link>
+          </Link> */}
         </div>
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
-              src="/banner.jpeg"
+              src="/banner3.jpeg"
               width={1280}
               height={720}
               alt="Dashboard Preview"
